de8bee34285ae2fefb7dcdaf31d833a45a2d752a
MachFrum <mpeter778@gmail.com>
2025-03-20 15:36:21 +0300

    Week 1 assignment

---
+++ answers.sql
@@ -0,0 +1,18 @@
+/*
+I created salesDB
+I then created demoDB
+Finally, I deleted, demoDB
+*/
+CREATE DATABASE salesDB;
+CREATE DATABASE demoDB;
+DROP DATABASE demodb;
+/*
+During Week 1, I encountered an issue in Visual Studio where my virtual environment was not activated. Normally, 
+the command prompt should display the prefix (venv), for example: (venv) C:\Users\Kingm\Downloads\lab1\lab-1> 
+Since it did not, my virtual environment was off. As a result, I did not create the database from the cloned repository 
+(https://github.com/PLP-Database-DEPT/lab-1.git) during the class activity; we only added the username, password, and database name manually.
+I'm not overly concerned at this point, but it would be great to resolve the issue so that I can edit code in Visual Studio 
+and push it to my SQL database. I'll continue working on this and will update you with any fixes in the Week 2 assignment.
+
+Thank you, and have an awesome day.
+*/
